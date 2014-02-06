6bfdbe7cc2d68f0628762da6601c5baeceed8f8f
emailbogsuv@gmail.com <emailbogsuv@gmail.com@180229ac-a0fa-1734-2b26-0a6ebbf42d13>
2014-02-06 04:02:52 +0000

    migrate from mt4 to mt5 update script mt, path to history data, format of history data

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -139,7 +139,7 @@ ListView_InsertColumn(hcmd,1,&lvc);
 //	server = new Server;
 //    if(action==optimizing)server->on(false);else server->on();
 
-    if(mode==999)wlog("Optimization: patterns.exe /MMCIS-Demo /opt /1440t\r\nTesting: patterns.exe /MMCIS-Demo /test /1440t\r\nDebuging: patterns.exe /MMCIS-Demo /debug /1440t\r\n\r\naction: /opt,/test,/debug\r\nperiod: /1t,/5t,/15t,/60t,/240t,/1440t,/10080t,/43200t\r\nmode: /MMCIS-Demo,/MMCIS-Real,/InstaForex-Demo.com\r\n\r\nExample: f:\\Program Files\\MMCIS MetaTrader 4 Client Terminal\\patterns.exe /MMCIS-Demo /opt /1440t\r\n");
+    if(mode==999)wlog("Optimization: patterns.exe /MMCIS-Demo /opt /1440t\r\nTesting: patterns.exe /MMCIS-Demo /test /1440t\r\nDebuging: patterns.exe /MMCIS-Demo /debug /1440t\r\n\r\naction: /opt,/test,/debug\r\nperiod: /1t,/5t,/15t,/60t,/240t,/1440t,/10080t,/43200t\r\nmode: /MMCIS-Demo,/MMCIS-Real,/InstaForex-Demo.com\r\n\r\nExample: mt5\\bases\\MetaQuotes-Demo\\history\\patterns.exe /MMCIS-Demo /opt /1440t\r\n");
 	else if(action!=optimizing)decode(action,period,mode,donottradecurrentbar,backbar);else decode(action,period,mode,tradecurrentbar,backbar);
     
     if(!find(lpszArgument,"/quit"))
